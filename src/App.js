88b08fe97ace420d09c32da3913aa9c7e3ef1c47
Matt Michel <mattmichel17@gmail.com>
2020-01-30 18:15:14 -0500

    don't know

--- src/App.js
+++ src/App.js
@@ -3,14 +3,16 @@ import logo from './logo.svg';
 import './App.css';
 import { TextField } from '@material-ui/core'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h1 style={{ color: 'red' }}>U of L SRC</h1>
-      </header>
+class App extends React.Component {
+  render() {
+    return(
+      <div className = "App" >
+        <header className="App-header">
+          <h1 style={{ color: 'red' }}>U of L SRC</h1>
+        </header>
     </div>
-  );
+    )
+  }
 }
 
 export default App;
